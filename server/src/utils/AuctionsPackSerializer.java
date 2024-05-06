bb9e3e5e1935847f67c47b0460fcbb42094a1608
fDero <francescodero@outlook.it>
2024-05-06 16:57:08 +0200

    Adapting the responses to match client-specific needs

--- server/src/utils/AuctionsPackSerializer.java
+++ server/src/utils/AuctionsPackSerializer.java
@@ -19,19 +19,41 @@ public class AuctionsPackSerializer extends JsonSerializer<AuctionsPack> {
     ) 
         throws IOException  
     {
+        gen.writeStartArray();
         for (Auction auction : value.getAuctions()) {
             gen.writeStartObject();
+            gen.writeStringField("id", auction.getId().toString());
             gen.writeStringField("title", auction.getItemName());
             gen.writeStringField("country", auction.getCountry());
             gen.writeStringField("city", auction.getCity());
             gen.writeStringField("conditions", auction.getItemCondition());
             gen.writeStringField("type", auction.getMacroCategory());
             if (auction.getMacroCategory().equals("REVERSE")) {
-                gen.writeNumberField("minimumBid", auction.getPrice());
-            } else {
-                gen.writeNumberField("maximumBid", auction.getPrice());
+                gen.writeObjectField("minimumBid", 
+                    new Object() {
+                        Number amount = auction.getPrice();
+                        String currency = "EUR";
+                    }
+                );
+            } else if (auction.getMacroCategory().equals("SILENT")) {
+                gen.writeObjectField("minimumBid", 
+                    new Object() {
+                        Number amount = auction.getPrice();
+                        String currency = "EUR";
+                    }
+                );
+                gen.writeObjectField("maximumBid", 
+                    new Object() {
+                        Number amount = auction.getPrice();
+                        String currency = "EUR";
+                    }
+                );
             }
             gen.writeStringField("endTime", auction.getEndTime().toString());
+            String[] pictureUrls = auction.getPicturesUrls();
+            String firstPictureUrl = pictureUrls.length > 0 ? pictureUrls[0] : null;
+            gen.writeStringField("pictureUrl", firstPictureUrl);
+            gen.writeStringField("status", "ACTIVE");
             gen.writeEndObject();
         }
         gen.writeEndArray();

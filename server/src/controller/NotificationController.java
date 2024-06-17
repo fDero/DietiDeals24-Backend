10b433ce9f1ef51f71ee39ac0f8f97883134a00d
fDero <francescodero@outlook.it>
2024-06-17 21:47:35 +0200

    Make '/notifications' use the email in the jwt token itself, hence remove the need for query parameters

--- server/src/controller/NotificationController.java
+++ server/src/controller/NotificationController.java
@@ -1,17 +1,19 @@
 package controller;
 
 import entity.Notification;
+import exceptions.NoAccountWithSuchEmailException;
 import repository.NotificationRepository;
 import response.NotificationsPack;
 
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import authentication.JwtTokenProvider;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
 import authentication.RequireJWT;
 
 
@@ -19,17 +21,22 @@ import authentication.RequireJWT;
 public class NotificationController {
     
     private final NotificationRepository notificationRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     public NotificationController(
-        NotificationRepository notificationRepository
+        NotificationRepository notificationRepository,
+        JwtTokenProvider jwtTokenProvider
     ) {
         this.notificationRepository = notificationRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @RequireJWT
     @GetMapping("/notifications")
-    public ResponseEntity<NotificationsPack> sendProfileInformations(@RequestParam String email) {
+    public ResponseEntity<NotificationsPack> sendProfileInformations(@RequestHeader(name = "Authorization") String authorizationHeader) {
+        String jwtToken = jwtTokenProvider.getTokenFromRequestHeader(authorizationHeader);
+        String email = jwtTokenProvider.getEmailFromJWT(jwtToken);
         List<Notification> notifications = notificationRepository.findByEmail(email);
         return ResponseEntity.ok().body(new NotificationsPack(notifications));
     }

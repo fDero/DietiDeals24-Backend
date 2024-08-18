2dcb9ff49e70e9cea022246eb71dd13700a34339
fDero <francescodero@outlook.it>
2024-08-18 17:36:08 +0200

    Make sure notifications marked as eliminated do not get sent to the client

--- server/src/repository/NotificationRepository.java
+++ server/src/repository/NotificationRepository.java
@@ -4,6 +4,7 @@ import entity.Notification;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     
-    List<Notification> findByAccountId(Integer username, Pageable pageable);
+    @Query("SELECT n FROM Notification n WHERE n.accountId = :accountId AND n.eliminated = false")
+    List<Notification> findByAccountId(Integer accountId, Pageable pageable);
+
     Optional<Notification> findById(Integer id);
 
     long countByAccountIdAndVisualizedAndEliminated(Integer accountId, boolean visualized, boolean eliminated);

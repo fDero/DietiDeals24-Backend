899b93f29a70d0a7e1c54b96288e842811df4114
fDero <francescodero@outlook.it>
2024-08-18 18:07:44 +0200

    Make 'GeographicalAwarenessService' cache the requests for 60 minutes

--- server/src/service/GeographicalAwarenessService.java
+++ server/src/service/GeographicalAwarenessService.java
@@ -4,7 +4,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -22,11 +24,21 @@ public class GeographicalAwarenessService {
     private final String key;
     private final Gson gson = new Gson();
 
+    List<GeographicalCountryDescriptor> europeanCountries;
+    Timestamp europeanCountriesLastUpdate = null;
+
+    HashMap<String, List<GeographicalCityDescriptor>> citiesByCountry = new HashMap<>();
+    HashMap<String, Timestamp> citiesByCountryLastUpdate = new HashMap<>();
+
     public GeographicalAwarenessService(@Value("${geo.api.key}") String key){
         this.key = key;
     }
-
+    
     public List<GeographicalCountryDescriptor> fetchEuropeanCountries() {
+        Timestamp oldestAcceptableUpateTimestamp = new Timestamp(System.currentTimeMillis() - 60 * 60);
+        if (europeanCountriesLastUpdate != null && europeanCountriesLastUpdate.after(oldestAcceptableUpateTimestamp)) {
+            return europeanCountries;
+        }
         try {
             HttpClient httpClient = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
@@ -38,7 +50,9 @@ public class GeographicalAwarenessService {
             String jsonString = response.body();
             System.out.println(jsonString);
             GeographicalCountryDescriptor[] countries = gson.fromJson(jsonString, GeographicalCountryDescriptor[].class);
-            return Arrays.asList(countries);
+            europeanCountries = Arrays.asList(countries);
+            europeanCountriesLastUpdate = new Timestamp(System.currentTimeMillis());
+            return europeanCountries;
         } catch (Exception e) {
             throw new RuntimeException("Error while fetching European countries", e);
         }
@@ -47,6 +61,10 @@ public class GeographicalAwarenessService {
     public List<GeographicalCityDescriptor> fetchCitiesFromCountry(String country_code)
         throws UnrecognizedCountryException 
     {
+        Timestamp oldestAcceptableUpateTimestamp = new Timestamp(System.currentTimeMillis() - 60 * 60);
+        if (citiesByCountryLastUpdate.containsKey(country_code) && citiesByCountryLastUpdate.get(country_code).after(oldestAcceptableUpateTimestamp)) {
+            return citiesByCountry.get(country_code);
+        }
         try {
             HttpClient httpClient = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
@@ -57,7 +75,10 @@ public class GeographicalAwarenessService {
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
             String jsonString = response.body();
             GeographicalCityDescriptor[] countries = gson.fromJson(jsonString, GeographicalCityDescriptor[].class);
-            return Arrays.asList(countries);
+            List<GeographicalCityDescriptor> res = Arrays.asList(countries);
+            citiesByCountry.put(country_code, res);
+            citiesByCountryLastUpdate.put(country_code, new Timestamp(System.currentTimeMillis()));
+            return res;
         }
         catch (Exception e) {
             throw new UnrecognizedCountryException();

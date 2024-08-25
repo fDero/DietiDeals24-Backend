b9dd5606d5ed97fb5f2bf561ec0cb57534064bc5
fDero <francescodero@outlook.it>
2024-08-25 12:34:28 +0200

    Reduce amount of informations needed for credit cards

--- server/src/request/NewCreditCreditCardRequest.java
+++ server/src/request/NewCreditCreditCardRequest.java
@@ -1,7 +1,5 @@
 package request;
 
-import java.sql.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +9,6 @@ import lombok.Setter;
 @Setter @AllArgsConstructor
 public class NewCreditCreditCardRequest extends NewPaymentMethodRequest {
 
-    private String cardNumber;
-    private Date   expirationDate;
-    private String name;
-    private String address;
-    private String city;
-    private String country;
-    private String zip;
+    private String paymentProcessorToken;
+    private Integer last4digits;
 }

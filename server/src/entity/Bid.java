ac67347c1a8e644814efd0316a7e82a8288c0177
fDero <francescodero@outlook.it>
2024-09-21 23:04:02 +0200

    Minor fix

--- server/src/entity/Bid.java
+++ server/src/entity/Bid.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -37,7 +36,6 @@ public class Bid {
     private Timestamp bidDate;
 
     @Id @Column(name = "bid_id")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_methods_seq")
-    @SequenceGenerator(name = "payment_methods_seq", sequenceName = "payment_methods_ids", allocationSize = 50, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 }
